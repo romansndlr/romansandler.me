b2b192becbd4c0569bacb39e84732632b8235c27
Roman Sandler <romansndlr@gmail.com>
2024-04-14 16:19:50 +0300

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,31 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun'
 import { logger } from 'hono/logger'
-import { Layout } from './components/layout'
 import { Mark } from './components/mark'
+import { jsxRenderer } from 'hono/jsx-renderer'
+import { Link } from './components/link'
 import './styles/main.css'
 
 const ONE_YEAR = 60 * 60 * 24 * 365
 const ONE_WEEK = 60 * 60 * 24 * 7
 const FIVE_MINUTES = 60 * 5
+
+const isDev = process.env.NODE_ENV === 'development'
+
+let mainStylesPath = ''
+
+// Only when running the Vite dev server we set the path to the source file
+if (isDev) {
+  mainStylesPath = '/src/styles/main.css'
+} else {
+  const viteManifest = await import('../dist/.vite/manifest.json')
+
+  const hashedPath = viteManifest.default['src/index.tsx'].css[0]
+
+  if (hashedPath) {
+    mainStylesPath = hashedPath
+  }
+}
 
 const app = new Hono()
 
@@ -24,6 +42,37 @@ app.use(
   serveStatic({ root: './dist' }),
 )
 
+app.use(
+  '*',
+  jsxRenderer(({ children }) => (
+    <html lang="en" className="min-h-full">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Roman Sandler</title>
+        {/* The only context where this shouldn't be applied in is Vite build time */}
+        {mainStylesPath && <link rel="stylesheet" href={mainStylesPath} />}
+      </head>
+      <body className="min-h-full antialiased dark:bg-black dark:text-white">
+        <div className="mx-auto flex min-h-full max-w-3xl flex-col py-10 px-6 md:py-14 md:px-8">
+          <header>
+            <h1 className="text-3xl font-semibold md:text-4xl">
+              <a href="/" aria-label="Home page">
+                Roman Sandler
+              </a>
+            </h1>
+          </header>
+          <main className="mt-6 flex-1">{children}</main>
+          <footer class="border-t pt-6">
+            This website is powered by <Link href="https://bun.sh/">Bun</Link>{' '}
+            and <Link href="https://hono.dev/">Hono</Link>
+          </footer>
+        </div>
+      </body>
+    </html>
+  )),
+)
+
 app.get(
   '/',
   async (c, next) => {
@@ -34,27 +83,24 @@ app.get(
 
     await next()
   },
-  (c) => {
-    return c.html(
-      <Layout>
-        <div class="space-y-6 text-2xl leading-relaxed">
-          <p>
-            Hi! I'm Roman Sandler, I am a former educator turned software
-            engineer from Tel Aviv, Israel.
-          </p>
-          <p>
-            I love everything about building software for the web, and I love
-            sharing everything I know about it.
-          </p>
-          <p>
-            I mostly work with <Mark>TypeScript</Mark>, <Mark>React</Mark>, and{' '}
-            <Mark>Tailwind CSS</Mark>, but I'm always playing around with new
-            and exciting technologies.
-          </p>
-        </div>
-      </Layout>,
-    )
-  },
+  (c) =>
+    c.render(
+      <div class="space-y-6 text-2xl leading-relaxed">
+        <p>
+          Hi! I'm Roman Sandler, I am a former educator turned software engineer
+          from Tel Aviv, Israel.
+        </p>
+        <p>
+          I love everything about building software for the web, and I love
+          sharing everything I know about it.
+        </p>
+        <p>
+          I mostly work with <Mark>TypeScript</Mark>, <Mark>React</Mark>, and{' '}
+          <Mark>Tailwind CSS</Mark>, but I'm always playing around with new and
+          exciting technologies.
+        </p>
+      </div>,
+    ),
 )
 
 export default {
